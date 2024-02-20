5b41822135456c41f328b76d61c7cda1dde91bbe
Semih Yazar <semih_114yazar@hotmail.com>
2024-02-20 22:41:05 +0300

    footerAddress mediator eklendi ve api controller'i eklendi

--- Core/CarBook.Application/Services/ServiceRegistiration.cs
+++ Core/CarBook.Application/Services/ServiceRegistiration.cs
@@ -3,8 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CarBook.Application.Services
 {
@@ -15,4 +17,27 @@ namespace CarBook.Application.Services
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
         }
     }
+
+/*
+    Mediator Service Registiration
+
+Core'un altındaki Application katmanında Mediator servicelerini tek tek program.cs'e yazmak yerine Application katmanında
+Services isminde klasör olusturduk.İçine ServiceRegistiration isminde class ekledik ve bu class içine   >>> "public static void AddApplicationService(this IServiceCollection service)" << isminde bir fonkiyon ekledik.Static olmasının sebebi: AddApplicationService metodunu kullanmak için örneğe ihtiyaç yoktur; sadece IServiceCollection türündeki bir nesne üzerinden çağrılır!! 
+
+	 Neden IServiceCollection Parametresi? (hala mediator service registiration)
+
+Bu metodun amacı, servisleri eklemek ve yapılandırmaktır.IServiceCollection, ASP.NET Core uygulamalarında servislerin kaydedildiği bir koleksiyonu temsil eder.Bu koleksiyona servisler eklenir, çıkarılır veya değiştirilir.Bu nedenle, bu metoda IServiceCollection parametresi verilerek, metodun bu koleksiyon üzerinde işlem yapabilmesi sağlanmış olur.
+
+
+    RegisterServicesFromAssembly Methodu Ne İşe Yarar?
+
+Yukarıda bahsedilen kod örneğinde, RegisterServicesFromAssembly metodu, belirli bir assembly içinde tanımlanan tüm servisleri kaydetmeyi sağlar.Bu, projenin içindeki tüm MediatR servislerini otomatik olarak tanımlar ve kaydeder.
+
+
+    Assembly Nedir ?
+
+"Assembly" terimi, .NET platformunda derlenmiş bir kodun birimini ifade eder. Bir assembly, genellikle bir DLL (Dynamic Link Library) veya bir EXE(Executable) dosyasıdır.Assembly'ler, bir veya birden fazla .NET dilinde yazılmış bir veya birden fazla tipi (class, interface, struct, vb.) içerir.
+
+Bir.NET uygulaması, bir veya birden fazla assembly içerebilir.Ayrıca, bir projenin derlenmesi sonucunda oluşturulan DLL veya EXE dosyaları da assembly olarak adlandırılır.
+*/
 }
---
+++ Presentation/CarBook.WepApi/Controllers/FooterAddressesController.cs
@@ -0,0 +1,56 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers;
+using CarBook.Application.Features.Mediator.Queries.FooterAddressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public FooterAddressesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressList()
+        {
+            var values = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FooterAddressList(int id)
+        {
+            var value = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand command, IMediator _mediator)
+        {
+            await _mediator.Send(command);
+            return Ok("Footer bilgisi eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+            await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok("Footer bilgisi silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Footer bilgisi güncellendi");
+        }
+        
+    }
+}
