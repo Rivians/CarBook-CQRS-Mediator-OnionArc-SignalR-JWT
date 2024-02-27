65151cff625ad59af2fd39ed4ffddffcde9d33c4
Semih Yazar <semih_114yazar@hotmail.com>
2024-02-27 23:21:50 +0300

    author ve blog entityleri eklendi -- author mediator eklendi ve apisi yazıldı

---
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }    
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int AuthorID { get; set; }
+        public Author Author { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
