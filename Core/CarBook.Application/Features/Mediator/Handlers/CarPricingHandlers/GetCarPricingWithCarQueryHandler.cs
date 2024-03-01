311765ccc5cec66f13f719a8a078d7c9d9eff32c
Semih Yazar <semih_114yazar@hotmail.com>
2024-03-01 17:20:32 +0300

    GetCarWithPricing cqrs- api - repositoryler yazıldı(hatalı) --- daha sonra CarPricing'e cevrildi - folder degisti - repo degisti

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithCarQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Application.Features.Mediator.Queries.CarPricingQueries;
+using CarBook.Application.Features.Mediator.Results.CarPricingResults;
+using CarBook.Application.Interfaces.CarPricingInterfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.CarPricingHandlers
+{
+    public class GetCarPricingWithCarQueryHandler : IRequestHandler<GetCarPricingWithCarQuery, List<GetCarPricingWithCarQueryResult>>
+    {
+        private readonly ICarPricingRepository _repository;
+        public GetCarPricingWithCarQueryHandler(ICarPricingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetCarPricingWithCarQueryResult>> Handle(GetCarPricingWithCarQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetCarPricingWithCars();
+            return values.Select(x => new GetCarPricingWithCarQueryResult
+            {
+                CarPricingId = x.CarPricingID,
+                Brand = x.Car.Brand.Name,
+                Model = x.Car.Model,
+                CoverImageUrl = x.Car.CoverImageUrl,
+                Amount = x.Amount
+            }).ToList();
+        }
+    }
+}
