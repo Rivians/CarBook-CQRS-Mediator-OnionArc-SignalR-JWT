8cfd33174f74ab28c79d4684160d10fbe3bec648
Semih Yazar <semih_114yazar@hotmail.com>
2024-03-15 05:22:41 +0300

    admin de araçların diger propları ile listelenmesi eklendi -- createCar action'u yazıldı tam bitmedi

--- FrontEnd/CarBook.WebUI/Controllers/AdminCarController.cs
+++ FrontEnd/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+		public AdminCarController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                return View(values);
+            }           
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CraeteCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Banners");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+			return View(values);
         }
     }
 }
--- FrontEnd/CarBook.WebUI/Controllers/BlogController.cs
+++ FrontEnd/CarBook.WebUI/Controllers/BlogController.cs
@@ -24,7 +24,7 @@ namespace CarBook.WebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
                 return View(values);
-            }s
+            }
             return View();
         }
 
