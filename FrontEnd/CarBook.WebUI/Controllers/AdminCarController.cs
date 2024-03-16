3c857c155558a945e7dd4f3f622de7922203493d
Semih Yazar <semih_114yazar@hotmail.com>
2024-03-16 23:55:54 +0300

    createCarDto eklendi -- adminCar controlleri içerisindeki CreateCar actionu yazıldı

--- FrontEnd/CarBook.WebUI/Controllers/AdminCarController.cs
+++ FrontEnd/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,7 @@
 ﻿using CarBook.Dto.BrandDtos;
 using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
@@ -27,13 +28,20 @@ namespace CarBook.WebUI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> CraeteCar()
+        public async Task<IActionResult> CreateCar()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7143/api/Banners");
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Brands");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
-			return View(values);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.BrandId.ToString(),
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+			return View();
         }
     }
 }
