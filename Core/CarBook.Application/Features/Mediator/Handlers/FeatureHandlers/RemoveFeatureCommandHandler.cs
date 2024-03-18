68633cc322e43437f9301c3a7e53ae66ced547a7
Semih Yazar <semih_114yazar@hotmail.com>
2024-03-18 20:16:15 +0300

    feature create-update-remove yazıldı ----  featureApi consume edildi

--- Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/RemoveFeatureCommandHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/RemoveFeatureCommandHandler.cs
@@ -12,8 +12,13 @@ namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
 {
     public class RemoveFeatureCommandHandler : IRequestHandler<RemoveFeatureCommand>
     {
-        private readonly IRepository<Feature> _repository; 
-        public async Task Handle(RemoveFeatureCommand request, CancellationToken cancellationToken)
+        private readonly IRepository<Feature> _repository;
+		public RemoveFeatureCommandHandler(IRepository<Feature> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(RemoveFeatureCommand request, CancellationToken cancellationToken)
         {
             var value = await _repository.GetByIdAsync(request.Id);
             await _repository.RemoveAsync(value);
--- Presentation/CarBook.WepApi/Controllers/FeaturesController.cs
+++ Presentation/CarBook.WepApi/Controllers/FeaturesController.cs
@@ -37,7 +37,7 @@ namespace CarBook.WepApi.Controllers
             return Ok("Özellik başarıyla eklendi.");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveFeature(int id)
         {
             await _mediator.Send(new RemoveFeatureCommand(id));
