83cef28949329d63c0b4fce3da8b3c9a4b09eb94
Semih Yazar <semih_114yazar@hotmail.com>
2024-05-06 01:57:22 +0300

    veriler dropdowna tasındı tempdatayla tasıdık

--- FrontEnd/CarBook.WebUI/Controllers/RentACarListController.cs
+++ FrontEnd/CarBook.WebUI/Controllers/RentACarListController.cs
@@ -6,6 +6,18 @@ namespace CarBook.WebUI.Controllers
     {
         public IActionResult Index()
         {
+            var bookPickDate = TempData["bookPickDate"];
+            var bookOffDate = TempData["bookOffDate"];
+            var timePick = TempData["timePick"];
+            var timeOff = TempData["timeOff"];
+            var locationID = TempData["locationID"];
+
+            ViewBag.bookPickDate = bookPickDate;
+            ViewBag.bookOffDate = bookOffDate;
+            ViewBag.timePick = timePick;
+            ViewBag.timeOff = timeOff;
+            ViewBag.locationID = locationID;
+
             return View();
         }
     }
