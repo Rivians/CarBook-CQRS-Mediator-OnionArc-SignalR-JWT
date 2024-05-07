d3dbfa7df3151af9a1d4974c51961329920be9a1
Semih Yazar <semih_114yazar@hotmail.com>
2024-05-08 01:48:08 +0300

    rentaCar index fixed

--- FrontEnd/CarBook.WebUI/Controllers/RentACarListController.cs
+++ FrontEnd/CarBook.WebUI/Controllers/RentACarListController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.RentACarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
     public class RentACarListController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public RentACarListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(int id)
         {
             var bookPickDate = TempData["bookPickDate"];
             var bookOffDate = TempData["bookOffDate"];
@@ -12,12 +21,24 @@ namespace CarBook.WebUI.Controllers
             var timeOff = TempData["timeOff"];
             var locationID = TempData["locationID"];
 
+            //filterRentACarDto.LocationID = int.Parse(locationID.ToString());
+            //filterRentACarDto.Available = true;
+            id = int.Parse(locationID.ToString());
+
             ViewBag.bookPickDate = bookPickDate;
             ViewBag.bookOffDate = bookOffDate;
             ViewBag.timePick = timePick;
             ViewBag.timeOff = timeOff;
             ViewBag.locationID = locationID;
 
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7143/api/RentACars?locationId={id}&available=true");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                string jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<FilterRentACarDto>>(jsonData);
+                return View(values);
+            }                
             return View();
         }
     }
--- Presentation/CarBook.WepApi/Controllers/RentACarsController.cs
+++ Presentation/CarBook.WepApi/Controllers/RentACarsController.cs
@@ -15,10 +15,16 @@ namespace CarBook.WepApi.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> GetRentACarListByLocation(GetRentACarQuery query)
+        [HttpGet]
+        public async Task<IActionResult> GetRentACarListByLocation(int locationId, bool available)
         {
-            var values = await _mediator.Send(query);
+            GetRentACarQuery getRentACarQuery = new GetRentACarQuery()
+            {
+                Available = available,
+                LocationID = locationId
+            };
+
+            var values = await _mediator.Send(getRentACarQuery);
             return Ok(values);
         }
     }
