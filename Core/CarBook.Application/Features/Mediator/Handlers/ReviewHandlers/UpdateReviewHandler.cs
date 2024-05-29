241e88f3ae8af141ba741998be2c47b699f511e7
Semih Yazar <semih_114yazar@hotmail.com>
2024-05-29 05:48:09 +0300

    mediator da review crud eklendi --- api tarafında post için fluent validation uygulandı

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ReviewHandlers/UpdateReviewHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ReviewCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+	public class UpdateReviewHandler : IRequestHandler<UpdateReviewCommand>
+	{
+		private readonly IRepository<Review> _repository;
+		public UpdateReviewHandler(IRepository<Review> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(UpdateReviewCommand request, CancellationToken cancellationToken)
+		{
+			var value = await _repository.GetByIdAsync(request.CarID);
+
+			value.ReviewDate = request.ReviewDate;
+			value.CustomerName = request.CustomerName;
+			value.CustomerImage = request.CustomerImage;
+			value.CarID = request.CarID;
+			value.RaytingValue = request.RaytingValue;
+			value.Comment = request.Comment;
+			
+			await _repository.UpdateAsync(value);
+		}
+	}
+}
---
+++ Core/CarBook.Application/Features/Validators/ReviewValidators/CreateReviewValidator.cs
@@ -0,0 +1,23 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ReviewCommands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Validators.ReviewValidators
+{
+	public class CreateReviewValidator : AbstractValidator<CreateReviewCommand>
+	{
+        public CreateReviewValidator()
+        {
+            RuleFor(x => x.CustomerName).NotEmpty().WithMessage("Lütfen müşteri adını boş geçmeyiniz.");
+            RuleFor(x => x.CustomerName).MinimumLength(3).WithMessage("Lütfen en az 3 karakterden oluşan bir veri girişi yapınız");
+            RuleFor(x => x.RaytingValue).NotEmpty().WithMessage("Lütfen puan değerini boş geçmeyiniz");
+            RuleFor(x => x.Comment).NotEmpty().WithMessage("Lütfen yorumu boş geçmeyiniz");
+            RuleFor(x => x.Comment).MinimumLength(10).WithMessage("Lütfen en az 10 karakterden oluşan bir veri girişi yapınız");
+            RuleFor(x => x.Comment).MaximumLength(500).WithMessage("Lütfen en fazla 500 karakterden oluşan bir veri girişi yapınız");
+        }
+    }
+}
